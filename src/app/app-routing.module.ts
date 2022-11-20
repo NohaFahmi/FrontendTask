15ea206883b2475d9bfd086a8690a069861c1a70
Noha M <nm1488@fayoum.edu.eg>
2022-11-20 18:57:49 +0200

    added the main modules and setup the routes for pages in app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('@modules/landing-home/landing-home.module').then(m => m.LandingHomeModule)
+  },
+  {
+    path: 'login',
+    pathMatch: 'full',
+    loadChildren: () => import('@modules/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'product/:id',
+    pathMatch: 'full',
+    loadChildren: () => import('@modules/product-details/product-details.module').then(m => m.ProductDetailsModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/landing-home/landing-home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {LandingHomeComponent} from "@modules/landing-home/landing-home.component";
+
+
+
+@NgModule({
+  declarations: [
+    LandingHomeComponent
+  ],
+  imports: [
+    CommonModule,
+  ]
+})
+export class LandingHomeModule { }
