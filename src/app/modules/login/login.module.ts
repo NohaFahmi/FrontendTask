24933eacfdc5f47bcca9210ea75def6e153730ee
Noha M <nm1488@fayoum.edu.eg>
2022-11-20 23:22:29 +0200

    added the login page layout styles & html

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import {NzInputModule} from "ng-zorro-antd/input";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {ReactiveFormsModule} from "@angular/forms";
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzIconModule} from "ng-zorro-antd/icon";
 
 
 @NgModule({
@@ -11,7 +16,12 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    NzInputModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzButtonModule,
+    NzIconModule
   ]
 })
 export class LoginModule { }
