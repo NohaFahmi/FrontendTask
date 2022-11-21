34f14fae48264aa05e836261cac9a64391627538
Noha M <nm1488@fayoum.edu.eg>
2022-11-22 00:59:46 +0200

    added the filters component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,23 +5,30 @@ import {NzButtonModule} from "ng-zorro-antd/button";
 import {NzIconModule} from "ng-zorro-antd/icon";
 import {NzRateModule} from "ng-zorro-antd/rate";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { ListFiltersComponent } from './components/list-filters/list-filters.component';
+import {NzCollapseModule} from "ng-zorro-antd/collapse";
+import {NzCheckboxModule} from "ng-zorro-antd/checkbox";
 
 
 
 @NgModule({
     declarations: [
-        ProductCardComponent
+        ProductCardComponent,
+        ListFiltersComponent
     ],
-    exports: [
-        ProductCardComponent
-    ],
+  exports: [
+    ProductCardComponent,
+    ListFiltersComponent
+  ],
   imports: [
     CommonModule,
     NzButtonModule,
     NzIconModule,
     NzRateModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    NzCollapseModule,
+    NzCheckboxModule
   ]
 })
 export class SharedModule { }
