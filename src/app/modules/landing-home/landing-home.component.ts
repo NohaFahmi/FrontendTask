5695fe717ec39c0379b8232c948ee88386404921
Noha M <nm1488@fayoum.edu.eg>
2022-11-23 01:13:53 +0200

    added get all products integration to landing home page

--- src/app/modules/landing-home/landing-home.component.ts
+++ src/app/modules/landing-home/landing-home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {IProduct} from "@interfaces/product.interface";
+import {ProductsService} from "@services/integrations/products/products.service";
 
 @Component({
   selector: 'app-landing-home',
@@ -8,10 +9,18 @@ import {IProduct} from "@interfaces/product.interface";
   encapsulation: ViewEncapsulation.None
 })
 export class LandingHomeComponent implements OnInit {
-  products?: IProduct[];
-  constructor() { }
+  products: IProduct[] = [];
+
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
+    this.getAllProductsList();
+  }
+
+  getAllProductsList() {
+    this.productsService.getAllProducts('landing-home').then((result) => {
+      this.products = result.products;
+    })
   }
 
 }
